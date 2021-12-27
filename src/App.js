1d46f291eedace7bfcf0142b71de4dde2e696179
Tish G <latisha.griffiths@gmail.com>
2021-12-27 17:06:20 -0600

    add title and image gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then((response) => response.json())
+      .then((data) => {
+        const photos = data.splice(0, 25);
+        setPhotos(photos);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Photo Gallery</h1>
+      <div className="container gallery">
+        <div className="grid">
+          {photos.length ? 
+            photos.map((photo) => (
+              <img
+                key={photo.title}
+                src={photo?.thumbnailUrl}
+                className="img-thumbnail border g-col"
+                alt={photo.title}
+              />
+            )
+          ) : <div className="container"><div class="spinner-grow text-success" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-danger" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-success" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-danger" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-success" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+</div> }
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
