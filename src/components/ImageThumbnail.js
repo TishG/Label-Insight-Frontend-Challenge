3875d1533fa069c6f6133a5d7451c82d9636ee38
Tish G <latisha.griffiths@gmail.com>
2021-12-28 08:06:07 -0600

    add Image Thumbnail components

---
+++ src/components/ImageThumbnail.js
@@ -0,0 +1,26 @@
+import { IMAGE_MODAL } from '../consts';
+
+const ImageThumbnail = ({
+  title,
+  thumbnailUrl,
+  url,
+  handleClick,
+}) => (
+  <button
+    onClick={() => handleClick(title, url)}
+    type="button"
+    className="btn col p-0 m-0"
+    data-bs-toggle="modal"
+    data-bs-target={`#${IMAGE_MODAL}`}
+  >
+    {(thumbnailUrl && (
+      <img
+        src={thumbnailUrl}
+        className="img-thumbnail border p-0"
+        alt={title || 'Title Unavailable'}
+      />
+    )) || <div>Image Unavailable</div>}
+  </button>
+);
+
+export default ImageThumbnail;
