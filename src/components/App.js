ac96805415a78bb7ca1ad09a239ff41fed54dfb6
Tish G <latisha.griffiths@gmail.com>
2021-12-27 23:31:37 -0600

    add modal to App

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,10 @@ const App = () => {
     src: '',
     description: '',
   });
+  const [isTyping, setIsTyping] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
   const fetchAndSetPhotos = async () => {
     try {
@@ -39,19 +41,49 @@ const App = () => {
   };
 
   const handleTileClick = (photo) => {
+    const savedDescriptions = localStorage.getItem('descriptions');
+    const descriptions =
+      savedDescriptions && JSON.parse(savedDescriptions);
+    const modalDescription = descriptions[photo.title] || '';
+
     const selectedPhoto = {
       title: photo.title,
       src: photo.url,
-      description: modal.description,
+      description: modalDescription,
     };
 
     setModal(selectedPhoto);
+    console.log(modalDescription);
   };
 
   useEffect(() => {
     fetchAndSetPhotos();
   }, []);
 
+  const handleChange = (e) => {
+    setIsTyping(true);
+    setModal({ ...modal, description: e.target.value });
+  };
+
+  const handleSave = (e) => {
+    const savedDescriptions = localStorage.getItem('descriptions');
+    const descriptions =
+      (savedDescriptions && JSON.parse(savedDescriptions)) || {};
+
+    descriptions[modal.title] = modal.description;
+    localStorage.setItem(
+      'descriptions',
+      JSON.stringify(descriptions),
+    );
+    setIsTyping(false);
+    setShowForm(false);
+  };
+
+  const handleCancelForm = () => {
+    setModal({ ...modal, description: '' });
+    setShowForm(false);
+  };
+  console.log({ description: modal.description }, isTyping);
   return (
     <div className="App">
       <Jumbotron title="My Photo Gallery" />
@@ -107,7 +139,51 @@ const App = () => {
                 className="img-fluid"
                 alt={modal.title}
               />
-              <div className="description">{modal.description}</div>
+              {(modal.description && !isTyping && (
+                <div className="description">{modal.description}</div>
+              )) ||
+                (!modal.description && !showForm && (
+                  <button
+                    type="button"
+                    className="btn btn-primary mt-1"
+                    onClick={() => setShowForm(true)}
+                  >
+                    Add a description
+                  </button>
+                )) ||
+                (showForm && (
+                  <div className="mb-3">
+                    <label
+                      htmlFor="exampleFormControlTextarea1"
+                      className="form-label"
+                    >
+                      Description
+                    </label>
+                    <textarea
+                      onChange={(e) => handleChange(e)}
+                      placeholder="At the Negril beach in Jamiaica with my family on a sunny, June day."
+                      className="form-control mb-1"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                    ></textarea>
+                    <div className="d-flex justify-content-end">
+                      <button
+                        type="button"
+                        className="btn btn-danger mx-1"
+                        onClick={() => handleCancelForm()}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="submit"
+                        className="btn btn-primary"
+                        onClick={() => handleSave()}
+                      >
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                ))}
             </div>
             <div className="modal-footer">
               <button
